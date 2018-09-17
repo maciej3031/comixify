86fbb5571d1792ed21424875d4d363365549d538
Adam Svystun <adam.svytun@gmail.com>
2018-09-18 01:05:00 +0300

    Update settings to use in memory db for tests

--- settings/settings.py
+++ settings/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,6 +89,8 @@ DATABASES = {
         'CONN_MAX_AGE': 60,
     }
 }
+if 'test' in sys.argv:
+    DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
