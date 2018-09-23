858f9590adafc4fa9ee2fdd4ab33603e45be727b
Adam Svystun <adam.svytun@gmail.com>
2018-09-23 15:39:24 +0300

    Make requested changes

--- keyframes/keyframes.py
+++ keyframes/keyframes.py
@@ -1,25 +1,19 @@
 import os
 from subprocess import call
-import shutil
 import uuid
-import cv2
-import argparse
-import sys
-import time
-import datetime
 import numpy as np
 import torch
+import torch.nn as nn
 from django.conf import settings
 import caffe
-import operator
-from math import ceil, floor
+from math import ceil
 
 from utils import jj
 from keyframes_rl.models import DSN
-from keyframes_rl.knapsack import knapsack_dp
-from keyframes.kts import cpd_nonlin, cpd_auto
+from keyframes.kts import cpd_auto
 
-class KeyFramesExtractor():
+
+class KeyFramesExtractor:
     @classmethod
     def get_keyframes(cls, video, gpu=settings.GPU):
         frames_paths, all_frames_tmp_dir = cls._get_all_frames(video)
@@ -56,38 +50,31 @@ class KeyFramesExtractor():
         if not caffe_root:
             print("Caffe root path not found.")
 
-        MODEL_FILE = caffe_root + "models/bvlc_googlenet/deploy.prototxt"
-        PRETRAINED = caffe_root + "models/bvlc_googlenet/bvlc_googlenet.caffemodel"
+        model_file = caffe_root + "models/bvlc_googlenet/deploy.prototxt"
+        pretrained = caffe_root + "models/bvlc_googlenet/bvlc_googlenet.caffemodel"
 
-        if not os.path.isfile(PRETRAINED):
+        if not os.path.isfile(pretrained):
             print("PRETRAINED Model not found.")
         if not gpu:
             caffe.set_mode_cpu()
-        net = caffe.Net(MODEL_FILE, PRETRAINED, caffe.TEST)
-
-        # resize input size as we have only one image per batch.
+        net = caffe.Net(model_file, pretrained, caffe.TEST)
         net.blobs["data"].reshape(1, 3, 224, 224)
 
         mu = np.load(caffe_root + "python/caffe/imagenet/ilsvrc_2012_mean.npy")
-        mu = mu.mean(1).mean(1)  # average over pixels to obtain the mean (BGR) pixel values
-        
-        # create transformer for the input called "data"
+        mu = mu.mean(1).mean(1)
         transformer = caffe.io.Transformer({"data": net.blobs["data"].data.shape})
-
-        transformer.set_transpose("data", (2,0,1))      # move image channels to outermost dimension
-        transformer.set_mean("data", mu)                # subtract the dataset-mean value in each channel
-        transformer.set_raw_scale("data", 255)          # rescale from [0, 1] to [0, 255]
-        transformer.set_channel_swap("data", (2,1,0))   # swap channels from RGB to BGR
+        transformer.set_transpose("data", (2, 0, 1))
+        transformer.set_mean("data", mu)
+        transformer.set_raw_scale("data", 255)
+        transformer.set_channel_swap("data", (2, 1, 0))
 
         features = []
         for frame in frames:
             transformed_image = transformer.preprocess("data", frame)
             net.blobs["data"].data[0] = transformed_image 
             net.forward()
-            # features from pool5 layer
             temp = net.blobs["pool5/7x7_s1"].data[0] 
             temp = temp.squeeze().copy()
-            temp *= 0.03661728635813100419730620095037 # temprorary normalisation constant
             features.append(temp)
         features = np.array(features)
         return features
@@ -138,8 +125,10 @@ class KeyFramesExtractor():
     @staticmethod
     def _get_segments(features):
         K = np.dot(features, features.T)
-        min_segments = ceil(K.shape[0] / 10)
+        n_frames = int(K.shape[0])
+        min_segments = int(ceil(n_frames / 10))
         min_segments = max(10, min_segments)
+        min_segments = min(n_frames - 1, min_segments)
         cps, scores = cpd_auto(K, min_segments, 1)
         change_points = [
             [0, cps[0] - 1]
@@ -147,8 +136,7 @@ class KeyFramesExtractor():
         frames_per_segment = [int(cps[0])]
         for j in range(0, len(cps) - 1):
             change_points.append([cps[j], cps[j + 1] - 1])
-            frames_per_segment.append(int(cps[j+1] - cps[j]))
+            frames_per_segment.append(int(cps[j + 1] - cps[j]))
         frames_per_segment.append(int(len(features) - cps[len(cps) - 1]))
         change_points.append([cps[len(cps) - 1], len(features) - 1])
         return change_points, frames_per_segment
-    
--- keyframes/kts/__init__.py
+++ keyframes/kts/__init__.py
@@ -1,5 +1,5 @@
 import numpy as np
-#from scipy import weave
+
 
 def calc_scatters(K):
     """
@@ -9,30 +9,21 @@ def calc_scatters(K):
     n = K.shape[0]
     K1 = np.cumsum([0] + list(np.diag(K)))
     K2 = np.zeros((n+1, n+1))
-    K2[1:, 1:] = np.cumsum(np.cumsum(K, 0), 1);
-    scatters = np.zeros((n, n));
-
+    K2[1:, 1:] = np.cumsum(np.cumsum(K, 0), 1)
     diagK2 = np.diag(K2)
-
-    i = np.arange(n).reshape((-1,1))
-    j = np.arange(n).reshape((1,-1))
-    scatters = (K1[1:].reshape((1,-1))-K1[:-1].reshape((-1,1))
-                - (diagK2[1:].reshape((1,-1)) + diagK2[:-1].reshape((-1,1)) - K2[1:,:-1].T - K2[:-1,1:]) / ((j-i+1).astype(float) + (j==i-1).astype(float)))
-    scatters[j<i]=0
-    #code = r"""
-    #for (int i = 0; i < n; i++) {
-    #    for (int j = i; j < n; j++) {
-    #        scatters(i,j) = K1(j+1)-K1(i) - (K2(j+1,j+1)+K2(i,i)-K2(j+1,i)-K2(i,j+1))/(j-i+1);
-    #    }
-    #}
-    #"""
-    #weave.inline(code, ['K1','K2','scatters','n'], global_dict = \
-    #    {'K1':K1, 'K2':K2, 'scatters':scatters, 'n':n}, type_converters=weave.converters.blitz)
-
+    i = np.arange(n).reshape((-1, 1))
+    j = np.arange(n).reshape((1, -1))
+    scatters = (
+            K1[1:].reshape((1, -1)) - K1[:-1].reshape((-1, 1)) - (
+                diagK2[1:].reshape((1, -1)) + diagK2[:-1].reshape((-1, 1))
+                - K2[1:, :-1].T - K2[:-1, 1:]
+            ) / ((j - i + 1).astype(float) + (j == i-1).astype(float))
+    )
+    scatters[j < i] = 0
     return scatters
 
-def cpd_nonlin(K, ncp, lmin=1, lmax=100000, backtrack=True, verbose=True,
-    out_scatters=None):
+
+def cpd_nonlin(K, ncp, lmin=1, lmax=100000, backtrack=True, verbose=True, out_scatters=None):
     """ Change point detection with dynamic programming
     K - square kernel matrix
     ncp - number of change points to detect (ncp >= 0)
@@ -48,61 +39,37 @@ def cpd_nonlin(K, ncp, lmin=1, lmax=100000, backtrack=True, verbose=True,
     (n, n1) = K.shape
     assert(n == n1), "Kernel matrix awaited."
 
-    assert(n >= (m + 1)*lmin)
-    assert(n <= (m + 1)*lmax)
+    assert(n >= (m + 1) * lmin)
+    assert(n <= (m + 1) * lmax)
     assert(lmax >= lmin >= 1)
 
     if verbose:
-        #print "n =", n
         print("Precomputing scatters...")
     J = calc_scatters(K)
 
-    if out_scatters != None:
+    if out_scatters is not None:
         out_scatters[0] = J
 
     if verbose:
         print("Inferring best change points...")
     # I[k, l] - value of the objective for k change-points and l first frames
-    I = 1e101*np.ones((m+1, n+1))
-    I[0, lmin:lmax] = J[0, lmin-1:lmax-1]
+    I = 1e101 * np.ones((m + 1, n + 1))
+    I[0, lmin:lmax] = J[0, lmin - 1:lmax - 1]
 
     if backtrack:
         # p[k, l] --- "previous change" --- best t[k] when t[k+1] equals l
-        p = np.zeros((m+1, n+1), dtype=int)
+        p = np.zeros((m + 1, n + 1), dtype=int)
     else:
-        p = np.zeros((1,1), dtype=int)
+        p = np.zeros((1, 1), dtype=int)
 
-    for k in range(1,m+1):
-        for l in range((k+1)*lmin, n+1):
-            tmin = max(k*lmin, l-lmax)
-            tmax = l-lmin+1
-            c = J[tmin:tmax,l-1].reshape(-1) + I[k-1, tmin:tmax].reshape(-1)
-            I[k,l] = np.min(c)
+    for k in range(1, m + 1):
+        for l in range((k + 1) * lmin, n + 1):
+            tmin = max(k * lmin, l - lmax)
+            tmax = l - lmin + 1
+            c = J[tmin:tmax, l - 1].reshape(-1) + I[k - 1, tmin:tmax].reshape(-1)
+            I[k, l] = np.min(c)
             if backtrack:
-                p[k,l] = np.argmin(c)+tmin
-
-    #code = r"""
-    ##define max(x,y) ((x)>(y)?(x):(y))
-    #for (int k=1; k<m+1; k++) {
-    #    for (int l=(k+1)*lmin; l<n+1; l++) {
-    #        I(k, l) = 1e100; //nearly infinity
-    #        for (int t=max(k*lmin,l-lmax); t<l-lmin+1; t++) {
-    #            double c = I(k-1, t) + J(t, l-1);
-    #            if (c < I(k, l)) {
-    #                I(k, l) = c;
-    #                if (backtrack == 1) {
-    #                    p(k, l) = t;
-    #                }
-    #            }
-    #        }
-    #    }
-    #}
-    #"""
-
-    #weave.inline(code, ['m','n','p','I', 'J', 'lmin', 'lmax', 'backtrack'], \
-    #    global_dict={'m':m, 'n':n, 'p':p, 'I':I, 'J':J, \
-    #    'lmin':lmin, 'lmax':lmax, 'backtrack': int(1) if backtrack else int(0)},
-    #    type_converters=weave.converters.blitz)
+                p[k, l] = np.argmin(c)+tmin
 
     # Collect change points
     cps = np.zeros(m, dtype=int)
@@ -110,14 +77,15 @@ def cpd_nonlin(K, ncp, lmin=1, lmax=100000, backtrack=True, verbose=True,
     if backtrack:
         cur = n
         for k in range(m, 0, -1):
-            cps[k-1] = p[k, cur]
-            cur = cps[k-1]
+            cps[k - 1] = p[k, cur]
+            cur = cps[k - 1]
 
     scores = I[:, n].copy()
     scores[scores > 1e99] = np.inf
     return cps, scores
 
-def cpd_auto(K, ncp, vmax, desc_rate=1, min_segments=10, **kwargs):
+
+def cpd_auto(K, ncp, vmax, desc_rate=15, min_segments=10, **kwargs):
     """Main interface
     Detect change points automatically selecting their number
         K       - kernel between each pair of frames in video
@@ -142,59 +110,16 @@ def cpd_auto(K, ncp, vmax, desc_rate=1, min_segments=10, **kwargs):
     (_, scores) = cpd_nonlin(K, m, backtrack=False, **kwargs)
 
     N = K.shape[0]
-    N2 = N*desc_rate  # length of the video before subsampling
+    N2 = N * desc_rate  # length of the video before subsampling
 
-    penalties = np.zeros(m+1)
+    penalties = np.zeros(m + 1)
     # Prevent division by zero (in case of 0 changes)
-    ncp = np.arange(1, m+1)
-    penalties[1:] = (vmax*ncp/(2.0*N2))*(np.log(float(N2)/ncp)+1)
+    ncp = np.arange(1, m + 1)
+    penalties[1:] = (vmax * ncp/(2.0 * N2)) * (np.log(float(N2) / ncp) + 1)
 
     costs = scores/float(N) + penalties
-    m_best = np.argmin(costs)
-    print(m_best, min_segments)
+    m_best = int(np.argmin(costs))
     m_best = max(min_segments, m_best)
+    m_best = min(m_best, N)
     (cps, scores2) = cpd_nonlin(K, m_best, **kwargs)
-
-    return (cps, scores2)
-
-
-# ------------------------------------------------------------------------------
-# Extra functions (currently not used)
-
-def estimate_vmax(K_stable):
-    """K_stable - kernel between all frames of a stable segment"""
-    n = K_stable.shape[0]
-    vmax = np.trace(centering(K_stable)/n)
-    return vmax
-
-
-def centering(K):
-    """Apply kernel centering"""
-    mean_rows = np.mean(K, 1)[:, np.newaxis]
-    return K - mean_rows - mean_rows.T + np.mean(mean_rows)
-
-
-def eval_score(K, cps):
-    """ Evaluate unnormalized empirical score
-        (sum of kernelized scatters) for the given change-points """
-    N = K.shape[0]
-    cps = [0] + list(cps) + [N]
-    V1 = 0
-    V2 = 0
-    for i in range(len(cps)-1):
-        K_sub = K[cps[i]:cps[i+1], :][:, cps[i]:cps[i+1]]
-        V1 += np.sum(np.diag(K_sub))
-        V2 += np.sum(K_sub) / float(cps[i+1] - cps[i])
-    return (V1 - V2)
-
-
-def eval_cost(K, cps, score, vmax):
-    """ Evaluate cost function for automatic number of change points selection
-    K      - kernel between all frames
-    cps    - selected change-points
-    score  - unnormalized empirical score (sum of kernelized scatters)
-    vmax   - vmax parameter"""
-
-    N = K.shape[0]
-    penalty = (vmax*len(cps)/(2.0*N))*(np.log(float(N)/len(cps))+1)
-    return score/float(N) + penalty
+    return cps, scores2
