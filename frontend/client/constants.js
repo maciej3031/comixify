159bf8ef0ee0dd69422eea66a7799056d1f83b82
adamsvystun <adam.svystun@gmail.com>
2018-10-01 16:17:48 +0200

    Add youtube frontend

--- frontend/client/constants.js
+++ frontend/client/constants.js
@@ -1,3 +1,4 @@
 export const COMIXIFY_API = "/comixify/";
+export const FROM_YOUTUBE_API = "/comixify/from_yt/";
 export const MAX_FILE_SIZE = 50000000;
 export const PERMITTED_VIDEO_EXTENSIONS = "video/*";
