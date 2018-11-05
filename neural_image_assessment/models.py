f078bf284d23e68bd2265d42d94f7dbf1bbc3f23
Pawel Andruszkiewicz <andrusza2@gmail.com>
2018-11-05 20:03:58 +0100

    Add GPU mem fraction to NIMA model

--- neural_image_assessment/models.py
+++ neural_image_assessment/models.py
@@ -12,21 +12,27 @@ class NeuralImageAssessment:
     def __init__(self):
         if not os.path.exists(MODEL_PATH):
             print("Model file does not exist.")
-        with tf.device('/CPU:0'):
-            self.model = load_model(MODEL_PATH)
+        config = tf.ConfigProto()
+        config.gpu_options.per_process_gpu_memory_fraction = 0.001
+        config.gpu_options.allow_growth = True
+        self.session = tf.Session(config=config)
+        with self.session.as_default():
+            with tf.device('/CPU:0'):
+                self.model = load_model(MODEL_PATH)
 
     def get_assessment_score(self, image_path):
-        target_size = (224, 224)
-        img = load_img(image_path, target_size=target_size)
-        x = img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x)
+        with self.session.as_default():
+            target_size = (224, 224)
+            img = load_img(image_path, target_size=target_size)
+            x = img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            x = preprocess_input(x)
 
-        scores = self.model.predict(x, batch_size=1, verbose=0)[0]
+            scores = self.model.predict(x, batch_size=1, verbose=0)[0]
         mean = NeuralImageAssessment.mean_score(scores)
-        
+
         return mean
-    
+
     @staticmethod
     def mean_score(scores):
         si = np.arange(1, 11, 1)
